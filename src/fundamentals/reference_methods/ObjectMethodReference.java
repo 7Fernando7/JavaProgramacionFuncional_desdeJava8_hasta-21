646ada60b9e1ca47108b8396c3e4d9a496072f84
Fernando <info_fotografia@icloud.com>
2025-07-14 11:37:19 +0200

    Referenciar por objetos

---
+++ src/fundamentals/reference_methods/ObjectMethodReference.java
@@ -0,0 +1,18 @@
+package fundamentals.reference_methods;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ObjectMethodReference {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(10);
+
+        IntStream repeat = IntStream.range(1,14);
+
+        repeat.forEach(numbers::add);
+
+        System.out.println(numbers);
+    }
+}
