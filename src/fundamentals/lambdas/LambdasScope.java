8f567687a2d45e36e53332f1465e3469d47cf8f2
Fernando <info_fotografia@icloud.com>
2025-07-14 11:11:22 +0200

    Scope de las variables en una expresión Lambda

---
+++ src/fundamentals/lambdas/LambdasScope.java
@@ -0,0 +1,17 @@
+package fundamentals.lambdas;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class LambdasScope {
+
+    public static void main(String[] args) {
+        AtomicInteger num = new AtomicInteger(10);
+
+        IntStream.range(1,10).forEach(i -> num.set(num.get() + i));
+
+        System.out.println(num.get());
+    }
+}
