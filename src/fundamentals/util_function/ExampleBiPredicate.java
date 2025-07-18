54e00d3f4d0b4e34ceb6bc1657305467ac84e024
Fernando <info_fotografia@icloud.com>
2025-07-18 10:23:27 +0200

    Implementando la interface BiPredicate

---
+++ src/fundamentals/util_function/ExampleBiPredicate.java
@@ -0,0 +1,14 @@
+package fundamentals.util_function;
+
+import java.util.function.BiPredicate;
+
+public class ExampleBiPredicate {
+
+    static BiPredicate<String, String> isEquals = (s1,s2) -> s1.concat(s2).equals("HelloWorld");
+
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Worl1d";
+        System.out.println(isEquals.test(s1,s2));
+    }
+}
