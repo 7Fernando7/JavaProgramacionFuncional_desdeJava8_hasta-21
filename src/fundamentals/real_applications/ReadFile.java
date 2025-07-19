03cdc375550ba298b0d94414c0f4f97f41f545c0
Fernando <info_fotografia@icloud.com>
2025-07-19 11:45:03 +0200

    Leyendo un archivo con Lambdas

---
+++ src/fundamentals/real_applications/ReadFile.java
@@ -0,0 +1,23 @@
+package fundamentals.real_applications;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ReadFile {
+
+    public static void main(String[] args) {
+
+        Path file = Paths.get("src/resources/lambdas.txt");
+
+        try(Stream<String> lines = Files.lines(file).onClose(()-> System.out.println("Closing read"))){
+
+            lines.forEach(System.out::println);
+
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+}
