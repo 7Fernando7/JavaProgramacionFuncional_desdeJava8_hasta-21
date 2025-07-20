ad4a7c59edffa516ae181765f233e4f85180102d
Fernando <info_fotografia@icloud.com>
2025-07-20 13:46:16 +0200

    Métodos para hacer match

--- src/main/java/fundamentals/streams/StreamFinalOperators.java
+++ src/main/java/fundamentals/streams/StreamFinalOperators.java
@@ -12,7 +12,10 @@ public class StreamFinalOperators {
         Stream<Videogame> videogames = Database.videogames.stream();
 
         //countOperator(videogames);
-        forEachOperator(videogames);
+       //forEachOperator(videogames);
+        //anyMatchOperator(videogames);
+        //allMatchOperator(videogames);
+        noneMatchOperator(videogames);
     }
 
     static void countOperator(Stream<Videogame> stream){
@@ -22,4 +25,25 @@ public class StreamFinalOperators {
     static void forEachOperator(Stream<Videogame> stream){
         stream.forEach(System.out::println);
     }
+
+    static void anyMatchOperator(Stream<Videogame> stream){
+       boolean r = stream.anyMatch(v  -> v.getTotalSold() > 1);
+
+        System.out.println(r);
+    }
+
+    static void allMatchOperator(Stream<Videogame> stream){
+        boolean r = stream.allMatch(v  -> v.getTotalSold() > 150);
+
+        System.out.println(r);
+    }
+
+    static void noneMatchOperator(Stream<Videogame> stream){
+        boolean r = stream.noneMatch(v  -> v.getReviews().isEmpty());
+
+        System.out.println(r);
+    }
+
+
+
 }
